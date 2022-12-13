5a3d5f85621ab47cb2aaad0fe54bba77a636aad6
Josh Darling <joshua.a.darling@gmail.com>
2022-12-13 14:44:36 -0700

    sumValues() passed

--- tree.js
+++ tree.js
@@ -15,21 +15,29 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let sum = 0;
+    const sumNode = [this.root];
+
+    while (sumNode.length) {
+      let currentVal = sumNode.pop();
+
+      if (currentVal === null) break;
+      sum += currentVal.val;
+      for (let child of currentVal.children) {
+        sumNode.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
-
-  }
+  countEvens() {}
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
-
-  }
+  numGreater(lowerBound) {}
 }
 
 module.exports = { Tree, TreeNode };
