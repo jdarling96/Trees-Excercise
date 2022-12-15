d402a0c742d63742d6244d70b37a0572e60cbfef
Josh Darling <joshua.a.darling@gmail.com>
2022-12-15 13:54:12 -0700

    areCousins() tests passed

--- binary-tree.js
+++ binary-tree.js
@@ -16,7 +16,7 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth(root=this.root) {
+  minDepth(root = this.root) {
     /* let queue = [this.root];
     if (this.root === null) return 0;
     let min = 1;
@@ -34,77 +34,97 @@ class BinaryTree {
       min++;
     }
     return min; */
-  if(root === null){
-    return 0
-  }
+    if (root === null) {
+      return 0;
+    }
 
-
-  return 1 + Math.min(this.maxDepth(root.left), this.maxDepth(root.right))
-
-
-
-
+    return 1 + Math.min(this.maxDepth(root.left), this.maxDepth(root.right));
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth(root=this.root) {
-    if(root === null){
-      return 0
+  maxDepth(root = this.root) {
+    if (root === null) {
+      return 0;
     }
-  
-    
 
-    return 1 + Math.max(this.maxDepth(root.left), this.maxDepth(root.right))
-  
-  
-    
+    return 1 + Math.max(this.maxDepth(root.left), this.maxDepth(root.right));
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-  maxSum(root=this.root) {
-    if(root === null) return 0
+  maxSum(root = this.root) {
+    if (root === null) return 0;
 
-    let leftSide = this.maxSum(root.left) + root.val
-    let rightSide = this.maxSum(root.right) + root.val
+    let leftSide = this.maxSum(root.left) + root.val;
+    let rightSide = this.maxSum(root.right) + root.val;
 
-    
-    if(root === this.root){
-      return leftSide + rightSide - root.val
+    if (root === this.root) {
+      return leftSide + rightSide - root.val;
     }
 
-    return Math.max(leftSide, rightSide)
+    return Math.max(leftSide, rightSide);
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
-    let queue = [this.root]
-    if(this.root === null) return null
-    let checkVal = this.root.val
-    while(queue.length){
-      let current = queue.shift()
-      if(current.val > lowerBound){
-        if(current.val < checkVal ){
-          checkVal = current.val
+    let queue = [this.root];
+    if (this.root === null) return null;
+    let checkVal = this.root.val;
+    while (queue.length) {
+      let current = queue.shift();
+      if (current.val > lowerBound) {
+        if (current.val < checkVal) {
+          checkVal = current.val;
         }
       }
-      if(current.left) queue.push(current.left)
-      if(current.right) queue.push(current.right)
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
     }
-    if(lowerBound === checkVal) return null
-    return checkVal
+    if (lowerBound === checkVal) return null;
+    return checkVal;
   }
 
   /** Further study!
    * areCousins(node1, node2): determine whether two nodes are cousins
    * (i.e. are at the same level but have different parents. ) */
 
-  areCousins(node1, node2) {}
+  areCousins(node1, node2) {
+    let queue = [this.root];
+
+    let level = 1;
+    let foundNode1 = null;
+    let foundNode2 = null;
+    while (queue.length) {
+      let levelLength = queue.length;
+      while (levelLength) {
+        let current = queue.shift();
+
+        if (current.left === node1 && current.right === node2) {
+          return false;
+        }
+        if (current === node1) foundNode1 = level;
+        if (current === node2) foundNode2 = level;
+        if (foundNode1 && foundNode2) {
+          if (foundNode1 === foundNode2) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right);
+
+        levelLength--;
+      }
+      level++;
+    }
+  }
 
   /** Further study!
    * serialize(tree): serialize the BinaryTree object tree into a string. */
